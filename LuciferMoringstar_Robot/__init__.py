ba4f832501fed08b42970cbeb9ef5acec3ad259a
colonelblac <108042864+colonelblac@users.noreply.github.com>
2022-07-17 18:36:58 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -11,19 +11,9 @@ from .Channel import (
 )
 
 HELP = """
-Basic Commads
 
-/start :- check am Alive
-
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+JOIN FOR LIVE FOOTBALL MATCHES:
+Filmcognito LIVE  : [Click Me](https://t.me/filmcognitolives)
 
 """
 
